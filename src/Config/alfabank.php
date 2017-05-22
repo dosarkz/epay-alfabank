391d015967c78a70e3921c91dd582d528fe38467
Yerzhan Hn <ashenov.e@gmail.com>
2017-05-22 16:06:15 +0600

    Update alfabank.php

--- src/Config/alfabank.php
+++ src/Config/alfabank.php
@@ -2,6 +2,7 @@
 return [
     'pay_test_mode' => true,
     'TEST_GETAWAY_URL' => 'https://test.paymentgate.ru/testpayment/rest/',
+    'GETAWAY_URL' => 'https://pay.alfabank.kz/payment/rest/',
     'RETURN_URL' => '/',
 
 ];
