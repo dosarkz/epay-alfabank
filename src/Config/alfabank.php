da0c8e2886f28f4a1368900ce29cf56e57361112
Yezhan Ashenov <ashenov.e@gmail.com>
2017-06-05 11:22:21 +0600

    update config

--- src/Config/alfabank.php
+++ src/Config/alfabank.php
@@ -3,6 +3,7 @@ return [
     'pay_test_mode' => true,
     'TEST_GETAWAY_URL' => 'https://web.rbsuat.com/ab/rest/',
     'GETAWAY_URL' => 'https://pay.alfabank.kz/payment/rest/',
+    'mobile_test_link' =>  'https://web.rbsuat.com/ab/merchants/globalpay_kz/mobile_payment_ru.html?mdOrder=',
     'RETURN_URL' => '/',
 
 ];
