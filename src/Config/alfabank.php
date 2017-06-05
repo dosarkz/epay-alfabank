5da7599deff0d5c405542202101ab8a8ded72d32
Yezhan Ashenov <ashenov.e@gmail.com>
2017-06-05 11:01:46 +0600

    update url test platform

--- src/Config/alfabank.php
+++ src/Config/alfabank.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'pay_test_mode' => true,
-    'TEST_GETAWAY_URL' => 'https://test.paymentgate.ru/testpayment/rest/',
+    'TEST_GETAWAY_URL' => 'https://web.rbsuat.com/ab/rest/',
     'GETAWAY_URL' => 'https://pay.alfabank.kz/payment/rest/',
     'RETURN_URL' => '/',
 
